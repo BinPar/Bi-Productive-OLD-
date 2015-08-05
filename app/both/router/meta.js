6d3114893129a74196d9b1d7c8d340e75ccb51e4
Ignacio Ferro Picón <nacho@binpar.com>
2015-08-05 13:11:26 +0200

    Carga Inicial
    Carga inicial del proyecto

---
+++ app/both/router/meta.js
@@ -0,0 +1,9 @@
+if(Meteor.isClient) {
+  Meta.config({
+      options: {
+        // Meteor.settings[Meteor.settings.environment].public.meta.title
+        title: 'Bi-Productive',
+        suffix: 'Sistema de producción de BinPar Team S.L.'
+      }
+  });
+}
