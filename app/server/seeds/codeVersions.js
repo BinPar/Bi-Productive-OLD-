b535419551c896fd0a372c29bab6de9d560e6573
Ignacio Ferro Picón <nacho@binpar.com>
2015-08-06 10:19:20 +0200

    Mejoras en la gestión de versiones

--- app/server/seeds/codeVersions.js
+++ app/server/seeds/codeVersions.js
@@ -1,7 +1,7 @@
 Meteor.startup(function() {
 
-  CodeVersions.remove({});
-  /*
+  CodeVersions.remove({Version: '0.12'});
+  
   if (CodeVersions.find({Version: '0.12'}).count() === 0) {    
     CodeVersions.insert({
       Version: '0.12',
@@ -16,7 +16,7 @@ Meteor.startup(function() {
       Details:    '<p>Tras la versión <strong>0.11</strong> identifico muchos problemas de base con el desarrollo en <a href="http://www.meteor.com">Meteor</a>, realizo el tutorial de <a href="http://es.discovermeteor.com/">discovermeteor.com</a> y re-estudio la <a href="http://docs.meteor.com/#/full/">documentación</a> para mejorar mi dominio de la plataforma.</p>' +
                   '<p>El <a href="http://github.differential.com/meteor-boilerplate/">Boilerplate</a> de <a href="http://differential.com/">Diferencial</a> ha acelerado el proceso, pero tengo la sensación de no controlar los entresijos de como esta funcionando (como si llegase a un proyecto ya construido sin conocerlo).</p>'
     });
-  }*/
+  }
 
   if (CodeVersions.find({Version: '0.11'}).count() === 0) {    
     CodeVersions.insert({
