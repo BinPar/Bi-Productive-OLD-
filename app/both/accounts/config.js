40af49c335fd751c0217ff4291c080d4e99a8893
voceses <ignacioferropicon@Ignacios-MacBook-Pro.local>
2015-08-14 13:36:52 +0200

    Small Fixes

--- app/both/accounts/config.js
+++ app/both/accounts/config.js
@@ -1,6 +1,4 @@
 AccountsTemplates.configureRoute('signIn', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('signUp', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('ensureSignedIn', {layoutTemplate: 'appLayout'});
-
 T9n.setLanguage("es_ES");
-
