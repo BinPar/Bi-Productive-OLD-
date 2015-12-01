b8f4fa43589a1c27eecb4f7f7c05bfd57662c0bb
Ignacio Ferro Picón <nacho@binpar.com>
2015-12-01 17:06:39 +0100

    Versiones

--- app/server/seeds/users.js
+++ app/server/seeds/users.js
@@ -1,15 +1,15 @@
 Meteor.startup(function() {
-  //Meteor.users.remove({});
-  if(!Meteor.users.find().count()) {
-    var info = {
-      username: 'DRNachio',
-      password: '1234',
-      email: 'drnachio@binpar.com',
-      profile: {name: 'Ignacio Ferro Picón'}
-    };
+	//Meteor.users.remove({});
+	if(!Meteor.users.find().count()) {
+		var info = {
+			username: 'DRNachio',
+			password: '1234',
+			email: 'drnachio@binpar.com',
+			profile: {name: 'Ignacio Ferro Picón'}
+		};
 
-    var id = Accounts.createUser(info);
+		var id = Accounts.createUser(info);
 
-    Roles.addUsersToRoles(id, ['superadmin','administración','manager','producción']);
-  }
+		Roles.addUsersToRoles(id, ['superadmin','administración','manager','producción']);
+	}
 });
